44f5100cd940b62c34894c6d2aaa8a8633ff16b3
mr6900 <mr6900@students.inghamisd.org>
2019-04-23 08:11:19 -0400

    aesd

--- sketch_mar25c/sketch_mar25c.ino
+++ sketch_mar25c/sketch_mar25c.ino
@@ -14,39 +14,40 @@
      28/9/2013
   */
 
-#define L1  8
-#define L2  9
-#define L3  10
-#define L4  11
-#define R5  29
-#define R6  27
-#define R7  25
-#define R8  23
+#define IN1  8
+#define IN2  9
+#define IN3  10
+#define IN4  11
+#define IN5  29
+#define IN6  27
+#define IN7  25
+#define IN8  23
 
-const int delayBetweenSteps = 0;
+const int delayBetweenSteps = 1000;
 const int anPin = 1;
 int StepsR = 0;
 int StepsL = 0;
-const int stepsPerTurn = 4095; 
-boolean DirectionL = true;
-boolean DirectionR = true;// gre
+const long stepsPerTurn = 4095; 
 const double Circumference = 7.853981634;
+const double turnCircumference = 21.9911485751;
+const double fullTurn = 360;
 unsigned long last_timeL= 0;
 unsigned long last_timeR= 0;
-unsigned long currentMicros;
+unsigned long currentMicrosL;
+unsigned long currentMicrosR;
 int steps_RemainL=0;
 int steps_RemainR=0;
 void setup()
 {
 Serial.begin(9600);
-pinMode(L1, OUTPUT); 
-pinMode(L2, OUTPUT); 
-pinMode(L3, OUTPUT); 
-pinMode(L4, OUTPUT);
-pinMode(R5, OUTPUT);
-pinMode(R6, OUTPUT);
-pinMode(R7, OUTPUT);
-pinMode(R8, OUTPUT);
+pinMode(IN1, OUTPUT); 
+pinMode(IN2, OUTPUT); 
+pinMode(IN3, OUTPUT); 
+pinMode(IN4, OUTPUT);
+pinMode(IN5, OUTPUT);
+pinMode(IN6, OUTPUT);
+pinMode(IN7, OUTPUT);
+pinMode(IN8, OUTPUT);
 analogReference(DEFAULT);
 // delay(1000);
 
@@ -55,341 +56,276 @@ void loop()
 {
  Serial.println(analogRead(anPin));
 
+ //delay(5000);
+ //go(100, true);
+ turnInPlace(70.0, false);
+ //go(100, true);
  
- if(analogRead(anPin) < 200){
+
+  
+ 
+ /*if(analogRead(anPin) < 200){
   go(1, true);
  }else{
-  go(1, true);
- }
+  go(1, false);
+ }*/
  
-//delay(1000);
+delay(2000);
 
 
 }
-bool rotateRXTurnsBack(bool clockwise){
-  DirectionR = clockwise; // Turning Clockwise
-    currentMicros = micros();
-    if((currentMicros-last_timeR)>=delayBetweenSteps){
-      //Serial.println("currentMicros: " + currentMicros);
-      stepperRB();
-      stepperLB();
-      last_timeR=currentMicros;
+bool rotateL(bool clockwise){
+    currentMicrosL = micros();
+   // Serial.print("currentMicros: ");
+   // Serial.print(currentMicrosL-last_timeL);
+   // Serial.print("delay: ");
+   // Serial.println(delayBetweenSteps);
+ 
+    if((currentMicrosL-last_timeL)>=delayBetweenSteps){
+      stepperL(clockwise);
+      last_timeL=currentMicrosL;
+      // Serial.print("lastTimeL: ");
+     // Serial.print(last_timeL);
       return true;
     }
     return false;
 }
-bool rotateRXTurnsForward(bool clockwise){
-  DirectionR = clockwise; // Turning Clockwise
-    currentMicros = micros();
-    if((currentMicros-last_timeR)>=delayBetweenSteps){
-      //Serial.println("currentMicros: " + currentMicros);
-      stepperRF();
-      stepperLF();
-      last_timeR=currentMicros;
+bool rotateR(bool clockwise){
+    currentMicrosR = micros();
+    if((currentMicrosR-last_timeR)>=delayBetweenSteps){
+      //Serial.println("currentMicros: " + currentMicrosR);
+      stepperR(clockwise);
+      last_timeR=currentMicrosR;
       return true;
     }
     return false;
 }
 void go(double Distance, boolean Direction)
 {
-  int stepsRemain= (int) (Distance*stepsPerTurn/Circumference);
-  currentMicros = micros();
-  last_timeL = currentMicros;
-  last_timeR = currentMicros;
-  bool retVal =false;
+  long stepsRemain= (long) (Distance*stepsPerTurn/Circumference);
+  currentMicrosL = micros();
+  currentMicrosR = currentMicrosL;
+  last_timeL = currentMicrosL;
+  last_timeR = currentMicrosR;
+
  
   while(stepsRemain > 0){
-    //Serial.println("steps: ");
-    //Serial.println(stepsRemain);
-    if(Direction == true)
-    {
-      retVal = retVal || rotateRXTurnsBack(Direction);
-      // Move forward
-    }else
-    {
-      retVal = retVal || rotateRXTurnsForward(Direction);
-      //Move backwards
-     }
-     if(retVal){
-        stepsRemain--;
-     }
+      bool retValR = false;
+     bool retValL =false;
+    
+    while(true){
+    //  delay(500);
+      //Serial.print("steps: ");
+      //Serial.print(stepsRemain);
+     // Serial.print("retValL");
+     // Serial.print(retValL);
+     // Serial.print("retValR");
+     // Serial.println(retValR);
+      if(Direction == true)
+      {
+        if(!retValR){
+          retValR =  rotateR(Direction);
+        }
+        if(!retValL){
+
+          retValL =  rotateL(!Direction);
+        }
+        // Move forward
+      }else
+      {
+        if(!retValR){
+          retValR =  rotateR(!Direction);
+        }
+        if(!retValL){
+          retValL =  rotateL(Direction);
+        }
+        //Move backwards
+       }
+       if(retValR && retValL){
+          stepsRemain--;
+          break;
+       }
+    }
      
   }
 }
 
-void turnInPlace(int Degree, boolean Direction)
+void turnInPlace(double Degree, boolean Direction)
 {
-  int stepsRemain= (int) (Distance*stepsPerTurn/Circumference);
-  currentMicros = micros();
-  last_timeL = currentMicros;
-  last_timeR = currentMicros;
-  bool retVal =false;
-  
-  if (Direction = true)
-  {
-     retVal = retVal || rotateRXTurnsForwards(Direction);
-    //Turn left 90 degrees
-  }else
-  {
-    retVal = retVal || rotateRXTurnsForward(Direction);
-      //Move backwards
-    //Turn right 90 degrees
+  long stepsRemain= (long) (Degree/fullTurn*turnCircumference*stepsPerTurn/Circumference);
+ currentMicrosL = micros();
+  currentMicrosR = currentMicrosL;
+  last_timeL = currentMicrosL;
+  last_timeR = currentMicrosR;
+
+ 
+  while(stepsRemain > 0){
+     bool retValR = false;
+     bool retValL =false;
+    
+    while(true){
+    //  delay(500);
+     //Serial.print("steps: ");
+     //Serial.print(stepsRemain);
+     // Serial.print("retValL");
+     // Serial.print(retValL);
+     // Serial.print("retValR");
+     // Serial.println(retValR);
+      if(Direction == true)
+      {
+        if(!retValR){
+          retValR =  rotateR(!Direction);
+        }
+        if(!retValL){
+
+          retValL =  rotateL(!Direction);
+        }
+        // Move forward
+      }else
+      {
+        if(!retValR){
+          retValR =  rotateR(!Direction);
+        }
+        if(!retValL){
+          retValL =  rotateL(!Direction);
+        }
+        //Move backwards
+       }
+       if(retValR && retValL){
+          stepsRemain--;
+          break;
+       }
+    }
+     
   }
-  if(retVal){
-        stepsRemain--;
-     }
 }
-void stepperLB(){
-    SetDirectionL();
-    switch(StepsL){
-       case 0:
-         digitalWrite(L1, LOW); 
-         digitalWrite(L2, LOW);
-         digitalWrite(L3, LOW);
-         digitalWrite(L4, HIGH);///////////////////////
-       break; 
-       case 1:
-         digitalWrite(L1, LOW); 
-         digitalWrite(L2, LOW);
-         digitalWrite(L3, HIGH);
-         digitalWrite(L4, HIGH);//////////////////////
-       break; 
-       case 2:
-         digitalWrite(L1, LOW); 
-         digitalWrite(L2, LOW);
-         digitalWrite(L3, HIGH);
-         digitalWrite(L4, LOW);/////////////////////
-       break; 
-       case 3:
-         digitalWrite(L1, LOW); 
-         digitalWrite(L2, HIGH);
-         digitalWrite(L3, HIGH);
-         digitalWrite(L4, LOW);/////////////////
-       break; 
-       case 4:
-         digitalWrite(L1, LOW); 
-         digitalWrite(L2, HIGH);
-         digitalWrite(L3, LOW);
-         digitalWrite(L4, LOW);///////////////
-       break; 
-       case 5:
-         digitalWrite(L1, HIGH); 
-         digitalWrite(L2, HIGH);
-         digitalWrite(L3, LOW);
-         digitalWrite(L4, LOW);
-       break; 
-         case 6:
-         digitalWrite(L1, HIGH); 
-         digitalWrite(L2, LOW);
-         digitalWrite(L3, LOW);
-         digitalWrite(L4, LOW);
-       break; 
-       case 7:
-         digitalWrite(L1, HIGH); 
-         digitalWrite(L2, LOW);
-         digitalWrite(L3, LOW);
-         digitalWrite(L4, HIGH);
-       break; 
-       default:
-         digitalWrite(L1, LOW); 
-         digitalWrite(L2, LOW);
-         digitalWrite(L3, LOW);
-         digitalWrite(L4, LOW);
-       break; 
+void stepperL(bool direction){
+   setDirectionL(direction);
+   switch(StepsL){
+   case 0:
+     digitalWrite(IN5, LOW); 
+     digitalWrite(IN6, LOW);
+     digitalWrite(IN7, LOW);
+     digitalWrite(IN8, HIGH);
+   break; 
+   case 1:
+     digitalWrite(IN5, LOW); 
+     digitalWrite(IN6, LOW);
+     digitalWrite(IN7, HIGH);
+     digitalWrite(IN8, HIGH);
+   break; 
+   case 2:
+     digitalWrite(IN5, LOW); 
+     digitalWrite(IN6, LOW);
+     digitalWrite(IN7, HIGH);
+     digitalWrite(IN8, LOW);
+   break; 
+   case 3:
+     digitalWrite(IN5, LOW); 
+     digitalWrite(IN6, HIGH);
+     digitalWrite(IN7, HIGH);
+     digitalWrite(IN8, LOW);
+   break; 
+   case 4:
+     digitalWrite(IN5, LOW); 
+     digitalWrite(IN6, HIGH);
+     digitalWrite(IN7, LOW);
+     digitalWrite(IN8, LOW);
+   break; 
+   case 5:
+     digitalWrite(IN5, HIGH); 
+     digitalWrite(IN6, HIGH);
+     digitalWrite(IN7, LOW);
+     digitalWrite(IN8, LOW);
+   break; 
+     case 6:
+     digitalWrite(IN5, HIGH); 
+     digitalWrite(IN6, LOW);
+     digitalWrite(IN7, LOW);
+     digitalWrite(IN8, LOW);
+   break; 
+   case 7:
+     digitalWrite(IN5, HIGH); 
+     digitalWrite(IN6, LOW);
+     digitalWrite(IN7, LOW);
+     digitalWrite(IN8, HIGH);
+   break; 
+   default:
+     digitalWrite(IN5, LOW); 
+     digitalWrite(IN6, LOW);
+     digitalWrite(IN7, LOW);
+     digitalWrite(IN8, LOW);
+   break; 
     }
 }
-void stepperRB(){
-    SetDirectionR();
-    //Serial.print("StepsR: ");
-    //Serial.print(StepsR);
-    switch(StepsR){
-       case 0:
-         digitalWrite(R5, HIGH); 
-         digitalWrite(R6, LOW);
-         digitalWrite(R7, LOW);
-         digitalWrite(R8, HIGH);
-       break; 
-       case 1:
-         digitalWrite(R5, HIGH); 
-         digitalWrite(R6, LOW);
-         digitalWrite(R7, LOW);
-         digitalWrite(R8, LOW);
-       break; 
-       case 2:
-         digitalWrite(L1, HIGH); 
-         digitalWrite(L2, HIGH);
-         digitalWrite(L3, LOW);
-         digitalWrite(L4, LOW);
-       break; 
-       case 3:
-         digitalWrite(L1, LOW); 
-         digitalWrite(L2, HIGH);
-         digitalWrite(L3, LOW);
-         digitalWrite(L4, LOW);
-       break; 
-       case 4:
-         digitalWrite(R5, LOW); 
-         digitalWrite(R6, HIGH);
-         digitalWrite(R7, HIGH);
-         digitalWrite(R8, LOW);
-       break; 
-       case 5:
-         digitalWrite(L1, LOW); 
-         digitalWrite(L2, LOW);
-         digitalWrite(L3, HIGH);
-         digitalWrite(L4, LOW);
-       break; 
-         case 6:
-         digitalWrite(R5, LOW); 
-         digitalWrite(R6, LOW);
-         digitalWrite(R7, HIGH);
-         digitalWrite(R8, HIGH);
-       break; 
-       case 7:
-         digitalWrite(R5, LOW); 
-         digitalWrite(R6, LOW);
-         digitalWrite(R7, LOW);
-         digitalWrite(R8, HIGH);
-       break; 
-       default:
-         digitalWrite(R5, LOW); 
-         digitalWrite(R6, LOW);
-         digitalWrite(R7, LOW);
-         digitalWrite(R8, LOW);
-       break; 
+void stepperR(bool direction){
+   setDirectionR(direction);
+   switch(StepsR){
+   case 0:
+     digitalWrite(IN1, LOW); 
+     digitalWrite(IN2, LOW);
+     digitalWrite(IN3, LOW);
+     digitalWrite(IN4, HIGH);
+   break; 
+   case 1:
+     digitalWrite(IN1, LOW); 
+     digitalWrite(IN2, LOW);
+     digitalWrite(IN3, HIGH);
+     digitalWrite(IN4, HIGH);
+   break; 
+   case 2:
+     digitalWrite(IN1, LOW); 
+     digitalWrite(IN2, LOW);
+     digitalWrite(IN3, HIGH);
+     digitalWrite(IN4, LOW);
+   break; 
+   case 3:
+     digitalWrite(IN1, LOW); 
+     digitalWrite(IN2, HIGH);
+     digitalWrite(IN3, HIGH);
+     digitalWrite(IN4, LOW);
+   break; 
+   case 4:
+     digitalWrite(IN1, LOW); 
+     digitalWrite(IN2, HIGH);
+     digitalWrite(IN3, LOW);
+     digitalWrite(IN4, LOW);
+   break; 
+   case 5:
+     digitalWrite(IN1, HIGH); 
+     digitalWrite(IN2, HIGH);
+     digitalWrite(IN3, LOW);
+     digitalWrite(IN4, LOW);
+   break; 
+     case 6:
+     digitalWrite(IN1, HIGH); 
+     digitalWrite(IN2, LOW);
+     digitalWrite(IN3, LOW);
+     digitalWrite(IN4, LOW);
+   break; 
+   case 7:
+     digitalWrite(IN1, HIGH); 
+     digitalWrite(IN2, LOW);
+     digitalWrite(IN3, LOW);
+     digitalWrite(IN4, HIGH);
+   break; 
+   default:
+     digitalWrite(IN1, LOW); 
+     digitalWrite(IN2, LOW);
+     digitalWrite(IN3, LOW);
+     digitalWrite(IN4, LOW);
+   break; 
     }
 }
-
-void stepperLF(){
-    SetDirectionL();
-    switch(StepsL){
-       case 0:
-         digitalWrite(L1, HIGH); 
-         digitalWrite(L2, LOW);
-         digitalWrite(L3, LOW);
-         digitalWrite(L4, HIGH);///////////////////////
-       break; 
-       case 1:
-         digitalWrite(L1, HIGH); 
-         digitalWrite(L2, LOW);
-         digitalWrite(L3, LOW);
-         digitalWrite(L4, LOW);//////////////////////
-       break; 
-       case 2:
-         digitalWrite(L1, HIGH); 
-         digitalWrite(L2, HIGH);
-         digitalWrite(L3, LOW);
-         digitalWrite(L4, LOW);/////////////////////
-       break; 
-       case 3:
-         digitalWrite(L1, LOW); 
-         digitalWrite(L2, HIGH);
-         digitalWrite(L3, LOW);
-         digitalWrite(L4, LOW);/////////////////
-       break; 
-       case 4:
-         digitalWrite(L1, LOW); 
-         digitalWrite(L2, HIGH);
-         digitalWrite(L3, HIGH);
-         digitalWrite(L4, LOW);///////////////
-       break; 
-       case 5:
-         digitalWrite(L1, LOW); 
-         digitalWrite(L2, LOW);
-         digitalWrite(L3, HIGH);
-         digitalWrite(L4, LOW);
-       break; 
-         case 6:
-         digitalWrite(L1, LOW); 
-         digitalWrite(L2, LOW);
-         digitalWrite(L3, HIGH);
-         digitalWrite(L4, HIGH);
-       break; 
-       case 7:
-         digitalWrite(L1, LOW); 
-         digitalWrite(L2, LOW);
-         digitalWrite(L3, LOW);
-         digitalWrite(L4, HIGH);
-       break; 
-       default:
-         digitalWrite(L1, LOW); 
-         digitalWrite(L2, LOW);
-         digitalWrite(L3, LOW);
-         digitalWrite(L4, LOW);
-       break; 
-    }
-}
-
-void stepperRF(){
-    SetDirectionR();
-    //Serial.print("StepsR: ");
-    //Serial.print(StepsR);
-    switch(StepsR){
-       case 0:
-         digitalWrite(R5, HIGH); 
-         digitalWrite(R6, LOW);
-         digitalWrite(R7, LOW);
-         digitalWrite(R8, HIGH);
-       break; 
-       case 1:
-         digitalWrite(R5, HIGH); 
-         digitalWrite(R6, LOW);
-         digitalWrite(R7, LOW);
-         digitalWrite(R8, LOW);
-       break; 
-       case 2:
-         digitalWrite(L1, HIGH); 
-         digitalWrite(L2, HIGH);
-         digitalWrite(L3, LOW);
-         digitalWrite(L4, LOW);
-       break; 
-       case 3:
-         digitalWrite(L1, LOW); 
-         digitalWrite(L2, HIGH);
-         digitalWrite(L3, LOW);
-         digitalWrite(L4, LOW);
-       break; 
-       case 4:
-         digitalWrite(R5, LOW); 
-         digitalWrite(R6, HIGH);
-         digitalWrite(R7, HIGH);
-         digitalWrite(R8, LOW);
-       break; 
-       case 5:
-         digitalWrite(L1, LOW); 
-         digitalWrite(L2, LOW);
-         digitalWrite(L3, HIGH);
-         digitalWrite(L4, LOW);
-       break; 
-         case 6:
-         digitalWrite(R5, LOW); 
-         digitalWrite(R6, LOW);
-         digitalWrite(R7, HIGH);
-         digitalWrite(R8, HIGH);
-       break; 
-       case 7:
-         digitalWrite(R5, LOW); 
-         digitalWrite(R6, LOW);
-         digitalWrite(R7, LOW);
-         digitalWrite(R8, HIGH);
-       break; 
-       default:
-         digitalWrite(R5, LOW); 
-         digitalWrite(R6, LOW);
-         digitalWrite(R7, LOW);
-         digitalWrite(R8, LOW);
-       break; 
-    }
-}
-void SetDirectionR(){
-  if(DirectionR){ StepsR++;}
-  if(!DirectionR){ StepsR--; }
+void setDirectionR(bool direction){
+  if(direction){StepsR++;}
+  else{StepsR--;}
   if(StepsR>7){StepsR=0;}
   if(StepsR<0){StepsR=7; }
 }
-void SetDirectionL(){
-  if(DirectionL){ StepsL++;}
-  if(!DirectionL){ StepsL--; }
+void setDirectionL(bool direction){
+  if(direction){StepsL++;}
+  else{StepsL--;}
   if(StepsL>7){StepsL=0;}
   if(StepsL<0){StepsL=7; }
 }
